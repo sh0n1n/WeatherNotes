622ffeee2fbb795b3b9109b0b596993c32e53478
Artem <dogmaster31072003@gmail.com>
2025-12-16 12:06:42 +0100

    For weather

--- WeatherNotes/ViewModels/NoteDetailViewModel.swift
+++ WeatherNotes/ViewModels/NoteDetailViewModel.swift
@@ -1,1 +1,33 @@
 import Foundation
+
+final class NoteDetailViewModel: ObservableObject {
+    let note: Note
+
+    init(note: Note) {
+        self.note = note
+    }
+
+    var title: String { note.text }
+    var dateText: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm"
+        return formatter.string(from: note.createdAt)
+    }
+
+    var temperatureText: String {
+        "\(Int(note.weather.temperature.rounded()))°C"
+    }
+
+    var descriptionText: String {
+        note.weather.description.capitalized
+    }
+
+    var cityText: String {
+        note.weather.cityName
+    }
+
+    var iconURL: URL? {
+        // OpenWeather
+        URL(string: "https://openweathermap.org/img/wn/\(note.weather.icon)@2x.png")
+    }
+}
